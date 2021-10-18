176254812c96acbbda5c6d689d24188248bf4d58
Chandra Kiran G <chandug1107@gmail.com>
2021-10-19 02:00:25 +0530

    crud operations implemeted

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskDetail, TaskList
+from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate
 
 urlpatterns=[
     path('',TaskList.as_view(),name='tasks'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('task-create/',TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>/',TaskDelete.as_view(),name='task-delete'),
 ]
